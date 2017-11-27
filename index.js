c59f36e62c39d14ba941d63445f92ebef0f21ada
Bianca Power <drbiancampower@gmail.com>
2017-11-27 20:45:19 +1100

    working rgb colour changer

--- index.js
+++ index.js
@@ -1,25 +1,81 @@
-class App extends React.Component {
+class ColourShower extends React.Component {
+
+  render() {
+    var c = this.props.colour
+    var myColour = 'rgb(' + c.red + ',' + c.green + ',' + c.blue + ')'
+    return(
+      <div style={{color: myColour,}}>{'Change me'}</div>
+    )
+  }
+}
+
+class NumberChanger extends React.Component {
   constructor() {
     super();
-    this.state = { count: 0 }
     this.handleClick = this.handleClick.bind(this)
   }
 
   handleClick(event) {
+    console.log(this.props.target)
     const change = parseInt(event.target.value);
-    this.setState(() => ({ count: this.state.count + change }))
+    let currentState = this.props.parent.state
+    currentState[this.props.id] += change
+    this.props.parent.setState(() => (currentState))
+    console.log(currentState)
+  }
+
+  render() {
+    return(
+      <div>
+        <button value={-10} onClick={this.handleClick}>
+          -
+        </button>
+        <span>{this.props.colour}</span>
+        <button value={10} onClick={this.handleClick}>
+          +
+        </button>
+      </div>
+    )
+  }
+}
+
+class ColourPicker extends React.Component {
+  constructor() {
+    super();
+    this.state = { red: 200, green: 100, blue: 100 }
+  }
+  render() {
+    return [
+      <ColourShower colour={this.state}/>,
+
+      <br></br>,
+
+      <NumberChanger colour={'RED'} id={'red'} parent={this}/>,
+
+      <br></br>,
+
+      <NumberChanger colour={'GREEN'} id={'green'} parent={this}/>,
+
+      <br></br>,
+
+      <NumberChanger colour={'BLUE'} id={'blue'} parent={this}/>
+    ]
+  }
+}
+
+class App extends React.Component {
+  constructor() {
+    super();
+    // this.handleClick = this.handleClick.bind(this)
   }
 
   render() {
     return (
+
       <h2>
-        <button value={-1} onClick={this.handleClick}>
-          -
-        </button>
-        <span>{this.state.count}</span>
-        <button value={1} onClick={this.handleClick}>
-          +
-        </button>
+
+        <ColourPicker />
+
       </h2>
     )
   }
